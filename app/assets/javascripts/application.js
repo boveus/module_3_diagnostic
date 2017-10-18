fc62d8777a170bcd6276b944924c914988f9550e
Brandon Stewart <boveus@gmail.com>
2017-10-18 10:30:36 -0600

    add initial javascript to compute distance

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,26 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
+}
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
--- spec/features/user_can_search_for_a_zip_code_spec.rb
+++ spec/features/user_can_search_for_a_zip_code_spec.rb
@@ -11,20 +11,18 @@ feature "User can search for results based on a zip code" do
 
     expect(current_url).to eq("http://www.example.com/search?utf8=%E2%9C%93&q=80203&commit=Locate")
 
-    expect(page).to have_content('a closest station')
-    expect(page).to have_content('another close station name')
-    expect(page).to have_content('type')
-    expect(page).to have_content('station name')
-    expect(page).to have_content('station address')
-    expect(page).to have_content('station fuel type')
-    expect(page).to have_content('station distance')
-    expect(page).to have_content('station access times')
+    expect(page).to have_content("Name: Wells Fargo Center")
+    expect(page).to have_content('Address: 1700 Sherman Street')
+    expect(page).to have_content('Fuel type(s): ELEC')
+    #fuel type code for now, add a hash to englishfy it later
+    expect(page).to have_content('Distance: ')
+    expect(page).to have_content("Access Times: MO: 12:00am-12:00am; TU: 12:00am-12:00am; WE: 12:00am-12:00am; TH: 12:00am-12:00am; FR: 12:00am-12:00am; SA: 12:00am-12:00am; SU: 12:00am-12:00am")
     end
   end
 end
 
 
-# Request URL 
+# Request URL
 # As a user
 # When I visit "/"
 # And I fill in the search form with 80203
